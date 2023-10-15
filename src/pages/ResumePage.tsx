f44f5320d253c9a9b5f58834080263afcbe9b489
sonjasviden <sonja.sviden@live.com>
2023-10-16 01:14:42 +0200

    changed cv

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -1,36 +1,39 @@
-import { Col, Container, Image, Row } from "react-bootstrap"
-import documentPdf from '../assets/sonjasviden_cv.pdf';
+import { Col, Container, Image, Row } from "react-bootstrap";
+import documentPdf from "../assets/CV.pdf";
 
 const Resume = () => {
+  return (
+    <div>
+      <Container className="py-4 resume-page">
+        <h1 className="py-4">Resumé</h1>
 
-    return (
-        <div>
-            <Container className="py-4 resume-page">
-                <h1 className="py-4">Resumé</h1>
+        <Row>
+          <Col>
+            <a target="_blank" rel="noreferrer" href={documentPdf}>
+              <p>
+                Go to pdf download
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-download"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                </svg>
+              </p>
+            </a>
 
-                <Row>
-                    <Col>
-                        <a target="_blank" rel="noreferrer" href={documentPdf}>
-                            <p>Go to pdf download
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    className="bi bi-download" viewBox="0 0 16 16">
-                                    <path
-                                        d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
-                                    <path
-                                        d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
-                                </svg>
-                            </p>
-                        </a>
+            <div className="resume">
+              <Image src="src/assets/CV.jpeg"></Image>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-                        <div className="resume">
-                            <Image src="src/assets/sonjasviden_CV.png"></Image>
-                        </div>
-
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
-}
-
-export default Resume
+export default Resume;
